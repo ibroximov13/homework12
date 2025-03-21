7223cdf2c16465ed3be93f507da25f6a6f7baac1
Xamel10n-tech <xamel10n.xs@gmail.com>
2025-03-21 11:15:59 +0500

    verify token

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,36 +1,32 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-const { User } = require("../model");
-
-dotenv.config();
-
-const verifyToken = async (req, res, next) => {
-    try {
-        const header = req.header("Authorization");
-        if (!header || !header.startsWith("Bearer ")) {
-            return res.status(401).json({ msg: "Invalid or missing Authorization header" });
+function verifyToken(roles) {
+    return (req, res, next) => {
+        let authHeader = req.header("Authorization");
+        // console.log(authHeader);
+        if (!authHeader) {
+            return res.status(400).send({message: "Token not found"});
         }
-
-        const token = header.split(" ")[1];
+        let token = authHeader?.split(" ")[1];
+        
+        if (authHeader.startsWith("Bearer Bearer ")) {
+            token = authHeader.split(" ")[2];
+        };
         if (!token) {
-            return res.status(401).json({ msg: "Empty token" });
+            return res.status(400).send({message: "Token not found"});
+        }
+        try {
+            let matchToken = jwt.verify(token, "nimadir2");
+            if (roles.includes(matchToken.role)) {
+                req.user = matchToken.id;
+                req.userRole = matchToken.role;
+                next()
+            } else {
+                res.status(400).send({message: "Unfortunately, you do not have admin rights."})
+            }
+        } catch (error) {
+            res.send(error)
         }
 
-        const accessSecret = process.env.JWT_SECRET || "nimadir2";
-        const data = jwt.verify(token, accessSecret);
-
-        console.log(data);
-        const user = await User.findByPk(data.id);
-        console.log(user);
-        if (!user) {
-            return res.status(401).json({ msg: "Not allowed" });
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(500).json({ msg: "Server error", error: error.message });
     }
 };
 
-module.exports = verifyToken;
+module.exports = verifyToken
